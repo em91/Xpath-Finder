fee17d4cff2579440f704d77a2fa5f9c9abd25af
em91 <zju.em91@gmail.com>
2013-07-05 08:47:34 +0800

    misc:增加注释

--- devtools.js
+++ devtools.js
@@ -11,20 +11,21 @@ chrome.devtools.panels.create("Xpath Finder", "FontPicker.png", "xpath.html", fu
         }
     });
 
+    //panel显示出来之后绑定事件
+    //@todo 貌似在html里的JS都不会执行，只能在这里绑定事件
     panel.onShown.addListener(function tmp( win ) {
         panel.onShown.removeListener( tmp ); // Run once only
      
-
-        var document = win.document,
+        var document = win.document, 
             inputArea = document.getElementById( "xpath" ),
             searchBtn = document.getElementById( "search" );
 
         searchBtn.onclick = function(){
-          port.postMessage( {
-            command: "findXpath",
-            xpath: inputArea.value,
-            tabId: chrome.devtools.inspectedWindow.tabId
-          });
+            port.postMessage( {
+                command: "findXpath", 
+                xpath: inputArea.value, 
+                tabId: chrome.devtools.inspectedWindow.tabId 
+            });
         }
     });
 });
