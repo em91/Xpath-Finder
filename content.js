7b824ec44c05b44adc30f0a19b6c1aafc5ddbbd8
em91 <zju.em91@gmail.com>
2013-07-05 08:30:22 +0800

    misc:对输入空的或者错误的xpath进行容错处理

--- content.js
+++ content.js
@@ -1,19 +1,29 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  var response = [],
-      element;
+    var response = [], 
+        element;
 
-  var previousElements = document.getElementsByClassName( "chromeXpathFinder" );
-  for( var l = previousElements.length, i = l; i; i-- ){
-    try{
-      previousElements[i - 1].className = previousElements[i - 1].className.replace("chromeXpathFinder", "");
-    }catch( exp ){
+    var previousElements = document.getElementsByClassName( "chromeXpathFinder" );
+    for( var l = previousElements.length, i = l; i; i-- ){
+        try{
+            previousElements[i - 1].className = previousElements[i - 1].className.replace("chromeXpathFinder", "");
+        }catch( exp ){
+
+        }
     }
-  }
 
-  var results = document.evaluate( request.xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+    //没有输入XPATH，只要把页面上的outline删除即可。
+    if( !request.xpath ){
+        return true;
+    }
 
-  for( var i = 0, l = results.snapshotLength; i < l; i++ ){
-    var element = results.snapshotItem(i);
-    element.className += " chromeXpathFinder";
-  }
+    try{
+        var results = document.evaluate( request.xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );
+
+        for( var i = 0, l = results.snapshotLength; i < l; i++ ){
+            var element = results.snapshotItem(i);
+            element.className += " chromeXpathFinder";
+        }
+    }catch( exp ){
+        
+    }
 });
