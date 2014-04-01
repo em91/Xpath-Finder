104a6eb3f3de261e5b81f00de48bf5d9540ee792
em91 <zju.em91@gmail.com>
2014-04-01 10:27:42 +0800

    页面结构调整；国际化处理；将原有的devtool里的处理逻辑转移到html里

--- devtools.js
+++ devtools.js
@@ -1,13 +1,11 @@
-String.prototype.format = function (){if(arguments.length==0){return this}var a=arguments;return this.replace(/\{(\d+)\}/g,function(b,c){if(a[c]===undefined){return"{"+c+"}"}return a[c]})} ;
-
 chrome.devtools.panels.create("Xpath Finder", "FontPicker.png", "i18n/" + chrome.i18n.getMessage( "@@ui_locale" ) + "/xpath.html", function(panel) {
-    var _window; // Going to hold the reference to panel.html's `window`
+    var _window;
 
     var data = [];
     var port = chrome.extension.connect( { name: "xpathFinder" } );
     port.onMessage.addListener(function( msg ) {
         if ( _window ) {
-            _window.renderResults( msg );
+            _window.render( msg );
         } else {
             data.push(msg);
         }
@@ -17,65 +15,12 @@ chrome.devtools.panels.create("Xpath Finder", "FontPicker.png", "i18n/" + chrome
     //@todo 貌似在html里的JS都不会执行，只能在这里绑定事件
     panel.onShown.addListener(function tmp( win ) {
         panel.onShown.removeListener( tmp ); // Run once only
-     
+        
         _window = win;
-        var document = win.document, 
-            inputArea = document.getElementById( "xpath" ),
-            searchBtn = document.getElementById( "search" ),
-            historyArea = document.getElementById( "js-history" );
-
-        searchBtn.onclick = function(){
-            port.postMessage( {
-                command: "findXpath", 
-                xpath: inputArea.value, 
-                tabId: chrome.devtools.inspectedWindow.tabId 
-            });
-        }
-
-        historyArea.onclick = function( event ){
-            var target = event.target;
-            if( target.tagName === "A" && target.id != "js-clearHistory"){
-                inputArea.value = target.innerHTML;
-                searchBtn.click();
-            } else if ( target.id == "js-clearHistory" ){
-                chrome.devtools.inspectedWindow.eval( "location.host", function( host ){
-                    port.postMessage( {
-                        command: "clearHistory",
-                        host: host
-                    });
-                })
-            }
-        }
-
-
-        win.renderResults = function( msg ){
-            xpaths = msg.history;
-            var history = document.getElementById( "js-history" ).getElementsByTagName( "ul" )[0];
-            history.innerHTML = "";
-
-            //重新渲染查询历史
-            for( var xpath in xpaths ){
-                history.innerHTML = "<li><a href='javascript:void(0)'>" + xpath + "</a></li>" + history.innerHTML;
-            }
-
-            if( !history.innerHTML ){
-                history.innerHTML = "<span>" + chrome.i18n.getMessage( 'nohistory' ) + "</span>";
-            }
-
-            //如果有结果返回，也渲染到UI
-            if( msg.count ){
-                document.getElementById( "js-result" ).innerHTML = chrome.i18n.getMessage( 'resultfound' ).format( msg.count );
-            } else {
-                document.getElementById( "js-result" ).innerHTML = chrome.i18n.getMessage( 'noresult' );
-            }
-        }
+        _window.port = port;
         
         chrome.devtools.inspectedWindow.eval( "location.host", function( host ){
             port.postMessage( { command: "getHistory",  host: host } );
         })
-
-        if( data.length > 0 ){
-            win.renderResults( data );
-        }
     });
 });
---
+++ panel.js
@@ -0,0 +1,132 @@
+;
+String.prototype.format = function (){if(arguments.length==0){return this}var a=arguments;return this.replace(/\{(\d+)\}/g,function(b,c){if(a[c]===undefined){return"{"+c+"}"}return a[c]})} ;
+;
+
+var secSearch, secHistory;
+var searchBtn, clearBtn;
+var secResult;
+var xpathInput;
+
+var $  = document.querySelector.bind( document ),
+	$$ = document.querySelectorAll.bind( document );
+
+
+/**
+ * 历史记录读取等
+ * @return {void} 
+ */
+function init(){
+	secSearch = $( '#js-search' );
+	secHistory = $( '#js-history' );
+	secResult = $( '#js-result' );
+	searchBtn = $( '#js-searchBtn' );
+	clearBtn = $( '#js-clearHistory' );
+	xpathInput = $( 'input[name=xpath]' )
+}
+
+
+/**
+ * 事件绑定
+ * @return {void} 
+ */
+function bind(){
+	searchBtn.addEventListener( 'click', onSearchBtnClick );
+	secHistory.addEventListener( 'click', onHistoryClick );
+}
+
+
+/**
+ * 查找按钮点击，向页面postMessage
+ * @return {void} 
+ */
+function onSearchBtnClick(){
+    port.postMessage( {
+        command: "findXpath", 
+        xpath: xpathInput.value, 
+        tabId: chrome.devtools.inspectedWindow.tabId 
+    });
+}
+
+
+/**
+ * 历史记录点击，包含清空和查询历史
+ * @param  {Event} event 
+ * @return {void}       
+ */
+function onHistoryClick( event ){
+	var target = event.target;
+
+    if( target.tagName === "A" && target.id != "js-clearHistory"){
+        xpathInput.value = target.innerHTML;
+        searchBtn.click();
+    } else if ( target.id == "js-clearHistory" ){
+        chrome.devtools.inspectedWindow.eval( "location.host", function( host ){
+            port.postMessage( {
+                command: "clearHistory",
+                host: host
+            });
+        })
+    }	
+}
+
+
+/**
+ * 历史记录渲染
+ * @param  {array} history 
+ * @return {void}         
+ */
+function renderHistory( history ){
+	var historyUl = $( '#js-history ul' );
+
+	var htmlArr = [];
+
+	for( var key in history ) {
+		var xpath = key;
+		var html = '<li><a href="javascript:void(0)">' + xpath + '</a></li>';
+		htmlArr.push( html );
+	};	
+
+	if( htmlArr.length ) {
+		historyUl.innerHTML = htmlArr.join( '' );
+	} else {
+		historyUl.innerHTML = '<span>' + chrome.i18n.getMessage( 'nohistory' ) + '</span>';
+	}
+}
+
+
+/**
+ * 渲染查询结果
+ * @return {void} 
+ */
+function renderResult( count ){
+	if( !count ) {
+		secResult.innerHTML = chrome.i18n.getMessage( 'noresult' );
+	} else {
+		secResult.innerHTML = chrome.i18n.getMessage( 'resultfound' ).format( count );
+	}
+}
+
+/**
+ * 渲染数据
+ * @param  	{object} msg 
+ * @example {"history":{"//div":5,"/div":4,"//div/span":1}}
+ * @return 	{void}     
+ */
+function render( msg ){
+	if( msg.history ) {
+		renderHistory( msg.history );
+	}
+
+	if( msg.count != undefined ) {
+		renderResult( msg.count );
+	}
+}
+
+
+onload = function(){
+	init();
+	bind();
+};
+
+onerror = function(){
+};
